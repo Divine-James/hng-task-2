81704f43981ff301b31379dac96ee45b312b4a46
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-04 20:00:47 +0100

    pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "../../components/Header";
+import Link from "../../components/Link";
+import Socials from "../../components/Socials";
+
+const index = () => {
+  return (
+    <>
+      <Header />
+      <Link id="twitter" redirect="_blank" link="https://twitter.com/0ejemeh">
+        Twitter Link
+      </Link>
+      <Link id="btn__zuri" redirect="_blank" link="https://training.zuri.team/">
+        Zuri Team
+      </Link>
+      <Link id="books" redirect="_blank" link="http://books.zuri.team/">
+        Zuri Books
+      </Link>
+
+      <Link
+        id="book__python"
+        redirect="_blank"
+        link="https://books.zuri.team/python-for-beginers?ref_id=divine-james"
+      >
+        Python Books
+      </Link>
+      <Link id="pitch" redirect="_blank" link="https://background.zuri.team/">
+        Background Check for Coders
+      </Link>
+      <Link
+        id="book__design"
+        redirect="_blank"
+        link="https://books.zuri.team/design-rules"
+      >
+        Design Books
+      </Link>
+      <Link id="contact" link="/contact">
+        Contact Me
+      </Link>
+      <Socials />
+    </>
+  );
+};
+
+export default index;
