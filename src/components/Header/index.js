8755cfe02ab1a6d751c681178bf085d46137dad0
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-04 10:27:40 +0100

    new files

---
+++ src/components/Header/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+
+function Profile() {
+  /*const [width] = React.useState(window.innerWidth);
+  const breakpoint = 768;*/
+  return (
+    <header className="header">
+      <div className="share-icon">
+        <img
+          src="img/share-avater-icon.svg"
+          alt="share icon"
+          className="share"
+        />
+        <img src="img/dotted-hamburger.svg" alt="menu icon" className="menu" />
+      </div>
+      <div className="user-profile">
+        <img
+          src="img/picture.jpg"
+          alt="user"
+          className="picture"
+          id="profile__img"
+        ></img>
+      </div>
+      <div className="username">
+        <h1 className="myname" id="twitter">
+          Divine James
+        </h1>
+        <h3 className="hide" id="slack">
+          Divine James
+        </h3>
+      </div>
+    </header>
+  );
+}
+export default Profile;
+/*
+ */
