fae3a6a35a6176c4a2af57557304c3b4450268a2
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-04 18:48:35 +0100

    social icons

---
+++ src/components/Socials/Socials.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./socials.css";
+
+function Socials() {
+  return (
+    <section className="socials">
+      <a href="https://slack.com/Divine-james" className="slack">
+        <img src="img/slacklogo.png" alt="slack" className="slack"></img>
+      </a>
+      <a href="https://github.com/Divine-James" className="github">
+        <img src="img/icon.svg" alt="github" className="github"></img>
+      </a>
+    </section>
+  );
+}
+export default Socials;
